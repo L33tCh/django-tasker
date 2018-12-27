8a2b669a3c6676f23d3877e74a8c84633ca2ab0b
L33tCh <l33tch@gmail.com>
2018-12-27 10:05:05 +0200

    Initial commit

---
+++ first/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+
+class LinksPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'links.html', context=None)
+
+
+def test(request):
+    return HttpResponse('Test success!')
