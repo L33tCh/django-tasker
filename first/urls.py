90a102b7b63605c541af4918f25da879feb7b7b0
Jeffrey Crewe-Brown <l33tch@gmail.com>
2019-01-07 07:47:35 +0200

    Detail, voting and results

--- first/urls.py
+++ first/urls.py
@@ -6,6 +6,9 @@ app_name = 'first'
 
 urlpatterns = [
     path('first/', views.links),
+    path('first/<int:question_id>', views.detail, name='detail'),
+    path('first/<int:question_id>/vote', views.vote, name='vote'),
+    path('first/<int:question_id>/results', views.results, name='results'),
     path('api/<int:question_id>/', views.jtest, name='jsontest'),
     path('api/questions/', views.questions),
     # url(r'^$', views.HomePageView.as_view()),
